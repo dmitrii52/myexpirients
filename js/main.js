da4f9da6ea89dd27be737c01ee0af4189245daac
Dmitrii <dmitrii.bar97@mail.ru>
2023-05-18 10:00:02 +0300

    first comit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const numbeOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+	count: numbeOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt('Сколько фильмов вы уже посмотрели?', ''),	
+	  b = prompt('На сколько оцените его ?', ''),
+	  c = prompt('Сколько фильмов вы уже посмотрели?', ''),	
+	  d = prompt('На сколько оцените его ?', '');
+
+	  personalMovieDB.movies[a] = b;
+	  personalMovieDB.movies[c] = d;
+
+	  console.log(personalMovieDB);
